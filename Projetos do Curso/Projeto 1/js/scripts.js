208736c6e492249da05bbdbd4abf67f1ca0572a4
felipepatente <felipepatente@yahoo.com.br>
2020-03-08 11:19:38 -0300

    Colocando a data dinâmica no footer

--- Projetos do Curso/Projeto 1/js/scripts.js
+++ Projetos do Curso/Projeto 1/js/scripts.js
@@ -109,10 +109,13 @@ document.getElementById("service-next").onclick = function(){
 }
 
 
-
-
 // Data Footer
 
+var ano_atual = new Date;
+
+ano_atual = ano_atual.getFullYear();
+
+document.getElementById("current_year").innerHTML = ano_atual;
 
 
   
