253e3a1dcfa09d4479df3a22a4feb47ef5d498ca
felipepatente <felipepatente@yahoo.com.br>
2020-03-22 09:51:13 -0300

    Pegando o conteudo de um select_Box

---
+++ curso_js/js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("mostrar_opcao").onclick = function(){
+
+    var campo_select = document.getElementById("options");
+    var indice_selecionado = campo_select.options.selectedIndex;
+    var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+    document.getElementById("opcao_selecionada").innerHTML = valor_selecionado;
+};
