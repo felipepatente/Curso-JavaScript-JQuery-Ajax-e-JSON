733c0ff35436e9f9622b0e6b25aba78a1608f37d
felipepatente <felipepatente@yahoo.com.br>
2020-03-28 10:28:12 -0300

    Manipulação de classes CSS

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -98,3 +98,19 @@ $("input[name='interesse']").change(function() {
     console.log(textos);
     
 });
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+$("#adicionar_classe").click(function(){
+    $("#paragrafo-classes").addClass("styling");
+});
+
+// Remover classe:
+$("#remover_classe").click(function(){
+    $("#paragrafo-classes").removeClass("styling");
+});
+
+// Alternar classe:
+$("#alternar_classe").click(function(){
+    $("#paragrafo-classes").toggleClass("styling");
+});
