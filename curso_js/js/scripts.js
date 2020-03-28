bc4a6057865134f25511c0669a224e25519d344c
felipepatente <felipepatente@yahoo.com.br>
2020-03-28 11:01:41 -0300

    Eventos

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -1,116 +1,135 @@
-$("#esconder").click(function(){
-    $(".exemplo").css("display","none");
-});
+$(document).ready(function(){
 
-var html = $("#paragrafo-html").html();
-var text = $("#paragrafo-html").text();
-console.log(html);
-console.log(text);
-$("#paragrafo-html").html("Testando");
-
-var url_link = $("#paragrafo-link").attr("href");
-console.log(url_link);
-
-$("#paragrafo-link").attr("href","http://www.udemy.com.br");
-
-var url_link = $("#paragrafo-link").attr("href");
-console.log(url_link);
-
-$("#mudar_imagem").click(function(){
-    $("#imagem_js").attr("src", "https://cdn.tutsplus.com/net/uploads/2013/12/managing-async-nodejs-retina-preview.png");
-    $("#mudar_imagem").hide();
-});
-
-// $("#paragrafo-empty").empty();
-
-$("#paragrafo-empty").remove();
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var lista = ["HTML","CSS","Javascript", "jQuery", "PHP"];
-
-$.each(lista, function( indice, valor ) {
-    console.log('O elemento de índice [' + indice + '] tem o valor de ' + valor);
-});
-  
-
-var pessoa = {
-    'nome': 'João Pedro',
-    'DN': '20/01/1990',
-    'CPF': '111.111.111-11'
-};
-
-$.each(pessoa, function( chave, valor ) {
-    console.log('O elemento de chave [' + chave + '] tem o valor de ' + valor);
-});
-
-
-//Meu objeto, meu exemplo
-var carro = {
-    "Cor": "Preto",
-    "Modelo": "Civic",
-    "Marca": "Honda"
-}
-
-$.each(carro, function(chave, valor){
-    console.log('O elemento de chave [' + chave + '] tem o valor de ' + valor);
-});
-
-var interesses = $("#interesses li");
-
-$.each(interesses, function(chave, valor){
-    console.log($(valor).text());    
-});
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var conteudo_input = $("#campo_nome").val();
-console.log(conteudo_input);
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-$("#options").change(function(){
-    var novo_selecionado = $('#options').find(":selected").text();
-    console.log(novo_selecionado);
-})
-
-var valor_radio_selecionado = $("input[name='genero']:checked").val();
-console.log(valor_radio_selecionado);
-
-$("input[name='genero']").change(function() {
-
-    var novo_radio_selecionado = $("input[name='genero']:checked").parent('span').text();;
-    console.log(novo_radio_selecionado);
-
-});
-
-$("input[name='interesse']").change(function() {
-
-    var checkboxes_selecionados = $("input[name='interesse']:checked");
-    var textos = [];
+    $("#esconder").click(function(){
+        $(".exemplo").css("display","none");
+    });
     
-    $.each(checkboxes_selecionados, function( index, value ) {
-        
-        textos.push($(value).parent("span").text());
+    var html = $("#paragrafo-html").html();
+    var text = $("#paragrafo-html").text();
+    console.log(html);
+    console.log(text);
+    $("#paragrafo-html").html("Testando");
+    
+    var url_link = $("#paragrafo-link").attr("href");
+    console.log(url_link);
+    
+    $("#paragrafo-link").attr("href","http://www.udemy.com.br");
+    
+    var url_link = $("#paragrafo-link").attr("href");
+    console.log(url_link);
+    
+    $("#mudar_imagem").click(function(){
+        $("#imagem_js").attr("src", "https://cdn.tutsplus.com/net/uploads/2013/12/managing-async-nodejs-retina-preview.png");
+        $("#mudar_imagem").hide();
+    });
+    
+    // $("#paragrafo-empty").empty();
+    
+    $("#paragrafo-empty").remove();
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    var lista = ["HTML","CSS","Javascript", "jQuery", "PHP"];
+    
+    $.each(lista, function( indice, valor ) {
+        console.log('O elemento de índice [' + indice + '] tem o valor de ' + valor);
+    });
+      
+    
+    var pessoa = {
+        'nome': 'João Pedro',
+        'DN': '20/01/1990',
+        'CPF': '111.111.111-11'
+    };
+    
+    $.each(pessoa, function( chave, valor ) {
+        console.log('O elemento de chave [' + chave + '] tem o valor de ' + valor);
+    });
+    
+    
+    //Meu objeto, meu exemplo
+    var carro = {
+        "Cor": "Preto",
+        "Modelo": "Civic",
+        "Marca": "Honda"
+    }
+    
+    $.each(carro, function(chave, valor){
+        console.log('O elemento de chave [' + chave + '] tem o valor de ' + valor);
+    });
+    
+    var interesses = $("#interesses li");
+    
+    $.each(interesses, function(chave, valor){
+        console.log($(valor).text());    
+    });
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var conteudo_input = $("#campo_nome").val();
+    console.log(conteudo_input);
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    $("#options").change(function(){
+        var novo_selecionado = $('#options').find(":selected").text();
+        console.log(novo_selecionado);
+    })
+    
+    var valor_radio_selecionado = $("input[name='genero']:checked").val();
+    console.log(valor_radio_selecionado);
+    
+    $("input[name='genero']").change(function() {
+    
+        var novo_radio_selecionado = $("input[name='genero']:checked").parent('span').text();;
+        console.log(novo_radio_selecionado);
     
     });
     
-    console.log(textos);
+    $("input[name='interesse']").change(function() {
     
-});
+        var checkboxes_selecionados = $("input[name='interesse']:checked");
+        var textos = [];
+        
+        $.each(checkboxes_selecionados, function( index, value ) {
+            
+            textos.push($(value).parent("span").text());
+        
+        });
+        
+        console.log(textos);
+        
+    });
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    $("#adicionar_classe").click(function(){
+        $("#paragrafo-classes").addClass("styling");
+    });
+    
+    // Remover classe:
+    $("#remover_classe").click(function(){
+        $("#paragrafo-classes").removeClass("styling");
+    });
+    
+    // Alternar classe:
+    $("#alternar_classe").click(function(){
+        $("#paragrafo-classes").toggleClass("styling");
+    }); 
 
-////////////////////////////////////////////////////////////////////////////////////////////////
+    $("#nome").on({
+        
+        keypress: function(){
+            $("#confirmar").css("display", "block");
+        }, 
 
-$("#adicionar_classe").click(function(){
-    $("#paragrafo-classes").addClass("styling");
-});
+        keydown: function(){
+           
+            var valor = $("#nome").val();
 
-// Remover classe:
-$("#remover_classe").click(function(){
-    $("#paragrafo-classes").removeClass("styling");
-});
-
-// Alternar classe:
-$("#alternar_classe").click(function(){
-    $("#paragrafo-classes").toggleClass("styling");
+            if(valor == ""){
+                $("#confirmar").css("display", "none"); 
+            }
+        }, 
+    });
 });
