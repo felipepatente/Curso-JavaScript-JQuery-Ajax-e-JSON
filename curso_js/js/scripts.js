f6eedff658c925d6f5c982aeca1a57b13d5f3e1b
felipepatente <felipepatente@yahoo.com.br>
2020-03-28 09:27:53 -0300

    Manipulação do conteúdo HTML

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -1,3 +1,26 @@
 $("#esconder").click(function(){
     $(".exemplo").css("display","none");
 });
+
+var html = $("#paragrafo-html").html();
+var text = $("#paragrafo-html").text();
+console.log(html);
+console.log(text);
+$("#paragrafo-html").html("Testando");
+
+var url_link = $("#paragrafo-link").attr("href");
+console.log(url_link);
+
+$("#paragrafo-link").attr("href","http://www.udemy.com.br");
+
+var url_link = $("#paragrafo-link").attr("href");
+console.log(url_link);
+
+$("#mudar_imagem").click(function(){
+    $("#imagem_js").attr("src", "https://cdn.tutsplus.com/net/uploads/2013/12/managing-async-nodejs-retina-preview.png");
+    $("#mudar_imagem").hide();
+});
+
+// $("#paragrafo-empty").empty();
+
+$("#paragrafo-empty").remove();
