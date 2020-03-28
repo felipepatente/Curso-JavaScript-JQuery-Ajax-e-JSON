86f7ff142411fe17d353755e482428659f614ef3
felipepatente <felipepatente@yahoo.com.br>
2020-03-28 11:34:22 -0300

    Métodos em Cadeia

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -148,14 +148,14 @@ $(document).ready(function(){
 
     $("#animar").click(function(){
 
-        $("#quadrado").animate({
-            width: '+=20px'
-        },500);
-
-        $("#quadrado").animate({
-            height: '+=20px'
-        }),500;
-
+        $("#quadrado")
+            .animate({width: '+=20px'},500)
+            .animate({height: '+=20px'},500, 
+                function() {
+                    $("#quadrado").css('background-color', 'green')
+                }
+            )
+            .animate({borderRadius: '20px'});
     });
 
 });
